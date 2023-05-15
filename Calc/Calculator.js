5a733853cb52e3c1d91005333655984d176561bd
Hemanth-siddani <131584825+Hemanth-siddani@users.noreply.github.com>
2023-05-15 11:07:24 +0530

    Add files via upload

---
+++ Calc/Calculator.js
@@ -0,0 +1,54 @@
+
+let Input=document.getElementById('Input');
+
+function Add_1() {
+   Input.value+="1";
+}
+function Add_2() {
+    Input.value+="2";
+}
+function Add_3() {
+    Input.value+="3";
+}
+function Add_4() {
+    Input.value+="4";
+}
+function Add_5() {
+    Input.value+="5";
+}
+function Add_6() {
+    Input.value+="6";
+}
+function Add_7() {
+    Input.value+="7";
+}
+function Add_8() {
+    Input.value=Input.value+"8";
+}
+function Add_9() {
+    Input.value+="9";
+}
+function Add_0() {
+    Input.value+="0";
+}
+function Add_Plus() {
+    Input.value+="+";
+}
+function Add_Minus() {
+    Input.value+="-";
+}
+function Add_Multiply() {
+    Input.value+="*";
+}
+function Add_Divide() {
+    Input.value+="/";
+}
+function Equal() {
+    Input.value=eval(Input.value);
+}
+function AC() {
+    Input.value='';
+}
+
+ 
+
